fffa130198068fd8e60107f1f8bb3f03a2d1a843
matthewgz <matthewgz241@gmail.com>
2021-09-18 23:14:50 -0500

    [Feature] - Setting Material UI (#1)

---
+++ pages/_document.tsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+import createEmotionServer from '@emotion/server/create-instance'
+import createEmotionCache from '../src/createEmotionCache'
+
+import theme from '../styles/theme'
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+          />
+          <meta name="theme-color" content={theme.palette.primary.main} />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500&display=swap"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+MyDocument.getInitialProps = async (ctx) => {
+  const originalRenderPage = ctx.renderPage
+
+  const cache = createEmotionCache()
+  const { extractCriticalToChunks } = createEmotionServer(cache)
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      // eslint-disable-next-line react/display-name
+      enhanceApp: (App) => (props) =>
+        (
+          <App
+            /* tslint:disable */
+            emotionCache={cache}
+            {...props}
+          />
+        ),
+    })
+
+  const initialProps = await Document.getInitialProps(ctx)
+
+  const emotionStyles = extractCriticalToChunks(initialProps.html)
+  const emotionStyleTags = emotionStyles.styles.map((style) => (
+    <style
+      data-emotion={`${style.key} ${style.ids.join(' ')}`}
+      key={style.key}
+      dangerouslySetInnerHTML={{ __html: style.css }}
+    />
+  ))
+
+  return {
+    ...initialProps,
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      ...emotionStyleTags,
+    ],
+  }
+}
