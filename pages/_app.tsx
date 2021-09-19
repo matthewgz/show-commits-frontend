0da83d5a6c26f0dff3b7f89093f6d4b317bd65ac
Matthew Rosell Felix <matthewgz241@gmail.com>
2021-09-18 23:13:54 -0500

    [Feature] - Setting Material UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,32 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import { CacheProvider, EmotionCache } from '@emotion/react'
+
+import theme from '../styles/theme'
+import createEmotionCache from '../src/createEmotionCache'
+
+const clientSideEmotionCache = createEmotionCache()
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache
 }
-export default MyApp
+
+export default function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <title>Show Commits</title>
+        <meta name="description" content="Show commits related this project" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  )
+}
