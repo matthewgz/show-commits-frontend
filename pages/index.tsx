79b0387de1c6bc03a03f771bdf3194ba2889caab
matthewgz <matthewgz241@gmail.com>
2021-09-19 16:45:05 -0500

    [Feature] - Adding table for show commits (#4)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,46 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import type { NextPage } from 'next'
 
 import Header from 'components/Header'
 import ChooseRepository from 'components/ChooseRepository'
-import { Repository } from 'utils/types'
+import Table from 'components/Table'
+import { Commit, Repository } from 'utils/types'
 
 interface Props {
   repositories: Repository[]
+  commits: Commit[]
 }
 
-const Home: NextPage<Props> = ({ repositories }) => {
-  const [repository, setRepository] = useState(repositories[0].name)
+const Home: NextPage<Props> = ({ repositories, commits: commitsProps }) => {
+  const firstRender = useRef(true)
+  const [commits, setCommits] = useState<Commit[]>(commitsProps)
+  const [repository, setRepository] = useState<string>(
+    repositories?.[0]?.name || '',
+  )
+
+  useEffect(() => {
+    if (firstRender.current) {
+      firstRender.current = false
+      return
+    }
+
+    const getCommits = async () => {
+      try {
+        const baseUrl = process.env.NEXT_PUBLIC_ENV_BACKEND_API
+        const commitsFetched = await fetch(
+          `${baseUrl}repositories/${repository}/commits`,
+        )
+        const commitsData: Commit[] = await commitsFetched.json()
+        setCommits(commitsData)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    if (repository) {
+      getCommits()
+    }
+  }, [repository])
 
   return (
     <div>
@@ -21,24 +51,39 @@ const Home: NextPage<Props> = ({ repositories }) => {
           value={repository}
           setValue={setRepository}
         />
+        <Table commits={commits} />
       </main>
     </div>
   )
 }
 
 export async function getServerSideProps() {
-  const baseUrl = process.env.NEXT_PUBLIC_ENV_BACKEND_API
-  const repositoriesFetched = await fetch(`${baseUrl}repositories`)
-  const repositoriesData: Repository[] = await repositoriesFetched.json()
+  try {
+    let commitsFetched
+    let commitsData: Commit[] = []
+    const baseUrl = process.env.NEXT_PUBLIC_ENV_BACKEND_API
+    const repositoriesFetched = await fetch(`${baseUrl}repositories`)
+    const repositoriesData: Repository[] = await repositoriesFetched.json()
 
-  if (!repositoriesData) {
-    return {
-      notFound: true,
+    if (repositoriesData?.[0]?.name) {
+      commitsFetched = await fetch(
+        `${baseUrl}repositories/${repositoriesData[0].name}/commits`,
+      )
+      commitsData = await commitsFetched.json()
     }
-  }
 
-  return {
-    props: { repositories: repositoriesData },
+    if (!repositoriesData) {
+      return {
+        repositories: [],
+        commits: [],
+      }
+    }
+
+    return {
+      props: { repositories: repositoriesData, commits: commitsData },
+    }
+  } catch (error) {
+    console.error(error)
   }
 }
 
---
+++ src/utils/constants.ts
@@ -0,0 +1,3 @@
+export const DEFAULT_LIMIT = 5
+export const DEFAULT_PAGE = 1
+export const ROWS_PER_PAGE = [5, 10, 25]
