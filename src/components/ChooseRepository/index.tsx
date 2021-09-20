9eb4d9e7a0bf42323d11612482271d2104ea2586
Matthew Rosell Felix <matthewgz241@gmail.com>
2021-09-19 19:24:05 -0500

    [Fix] - Fixing typos and adding a loader

--- src/components/ChooseRepository/index.tsx
+++ src/components/ChooseRepository/index.tsx
@@ -1,6 +1,11 @@
 import { useCallback, Children, Dispatch, SetStateAction, memo } from 'react'
 
-import { ToggleButtonGroup, ToggleButton, Box, Button } from '@mui/material'
+import {
+  ToggleButtonGroup,
+  ToggleButton,
+  Box,
+  CircularProgress,
+} from '@mui/material'
 
 import { Repository } from 'utils/types'
 
@@ -8,36 +13,44 @@ interface Props {
   repositories: Repository[]
   value: string
   setValue: Dispatch<SetStateAction<string>>
+  loading: boolean
 }
 
-const ChooseRepository = memo(({ repositories, value, setValue }: Props) => {
-  const handleChange = useCallback(
-    (_: React.MouseEvent<HTMLElement>, newValue: string) => {
-      if (newValue) setValue(newValue)
-    },
-    [setValue],
-  )
+const ChooseRepository = memo(
+  ({ repositories, value, setValue, loading }: Props) => {
+    const handleChange = useCallback(
+      (_: React.MouseEvent<HTMLElement>, newValue: string) => {
+        if (newValue) setValue(newValue)
+      },
+      [setValue],
+    )
 
-  const renderToogleButton = useCallback(
-    (repository: Repository) => (
-      <ToggleButton value={repository.name}>{repository.label}</ToggleButton>
-    ),
-    [],
-  )
+    const renderToogleButton = useCallback(
+      (repository: Repository) => (
+        <ToggleButton value={repository.name}>
+          {repository.label}
+          {loading && value === repository.name ? (
+            <CircularProgress size={16} sx={{ marginLeft: 1 }} />
+          ) : null}
+        </ToggleButton>
+      ),
+      [loading, value],
+    )
 
-  return (
-    <Box display="flex" justifyContent="center" mt={4}>
-      <ToggleButtonGroup
-        color="primary"
-        value={value}
-        exclusive
-        onChange={handleChange}
-      >
-        {Children.toArray(repositories.map(renderToogleButton))}
-      </ToggleButtonGroup>
-    </Box>
-  )
-})
+    return (
+      <Box display="flex" justifyContent="center" mt={4}>
+        <ToggleButtonGroup
+          color="primary"
+          value={value}
+          exclusive
+          onChange={handleChange}
+        >
+          {Children.toArray(repositories.map(renderToogleButton))}
+        </ToggleButtonGroup>
+      </Box>
+    )
+  },
+)
 
 ChooseRepository.displayName = 'ChooseRepository'
 
